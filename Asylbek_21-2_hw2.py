ddf0ed93911b05c9bcead8b876c779b4adaf9f7d
ismail0vv <110819132+ismail0vv@users.noreply.github.com>
2022-08-10 18:01:53 +0600

    Update Asylbek_21-2_hw2.py

--- Asylbek_21-2_hw2.py
+++ Asylbek_21-2_hw2.py
@@ -23,9 +23,6 @@ class Figure:
 
 class Square(Figure):
 
-    def calculate_perimeter(self):
-        return self.__side_length * 4
-
     def __init__(self, side_length):
         super().__init__()
         self.__side_length = int(side_length)
@@ -34,14 +31,14 @@ class Square(Figure):
     def calculate_area(self):
         return self.__side_length ** 2
 
+    def calculate_perimeter(self):
+        return self.__side_length * 4
+
     def info(self):
         print(f'Square side length: {str(self.__side_length) + self.unit}, perimeter:'
               f'{str(self.perimeter) + self.unit}, area: {str(self.calculate_area()) + self.unit}.')
 
 class Rectangle(Figure):
-
-    def calculate_perimeter(self):
-        return self.__length * 2 + self.__width * 2
 
     def __init__(self, length, width):
         super().__init__()
@@ -51,6 +48,9 @@ class Rectangle(Figure):
 
     def calculate_area(self):
         return self.__length * self.__width
+
+    def calculate_perimeter(self):
+        return self.__length * 2 + self.__width * 2
 
     def info(self):
         print(f'Rectangle length: {str(self.__length) + self.unit}, width: {str(self.__width) + self.unit}, '
